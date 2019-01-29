6a069572136ea29553a3c020ac857fe4eec5da47
Matheus Kruger <ms-kruger@hotmail.com>
2019-01-29 17:15:52 -0200

    arrumando a bagunça

--- make.bat
+++ make.bat
@@ -1,2 +1,9 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+:: compiling the entire thing
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o build/main.o main.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o build/main.gb build/main.o
+
+:: cleaning up the mess
+rm build/main.lst
+rm build/main.map
+rm build/main.o
+rm build/main.sym
